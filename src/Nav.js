d75343e542a1ff4caa510cfe23429cb8579d1435
Austin Amani <amaniaustin123@gmail.com>
2023-11-16 17:11:17 +0300

    Worked on the Header,Nav and Home components

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
-const Nav = () => {
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+const Nav = ({ search, setSearch }) => {
 	return (
-		<nav>
-			<h1>Nav</h1>
+		<nav className="Nav">
+			<form className="SearchForm" onSubmit={(e) => e.preventDefault()}>
+				<label htmlFor="search">Search Posts</label>
+				<input
+					id="search"
+					type="text"
+					required
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/post">Post</Link>
+					</li>
+					<li>
+						<Link to="about">About</Link>
+					</li>
+				</ul>
+			</form>
 		</nav>
 	);
 };
