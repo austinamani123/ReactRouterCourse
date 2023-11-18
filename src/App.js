ae58d6cf2b45ab06a978799d59e1b8fa54834804
Austin Amani <amaniaustin123@gmail.com>
2023-11-18 11:43:08 +0300

    Programmed all components and edited the CSS

--- src/App.js
+++ src/App.js
@@ -8,36 +8,69 @@ import About from './About';
 import Missing from './Missing';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
 function App() {
 	const [search, setSearch] = useState('');
 	const [searchResults, setSearchResults] = useState([]);
+	const [postTitle, setPostTitle] = useState('');
+	const [postBody, setPostBody] = useState('');
 	const [posts, setPosts] = useState([
 		{
 			id: 1,
 			title: 'My First Post',
-			datetime: 'July 01, 2021 11:17:36 AM',
+			datetime: 'November 17, 2023 11:17:36 AM',
 			body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
 		},
 		{
 			id: 2,
 			title: 'My 2nd Post',
-			datetime: 'July 01, 2021 11:17:36 AM',
+			datetime: 'November 17, 2023 11:17:36 AM',
 			body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
 		},
 		{
 			id: 3,
 			title: 'My 3rd Post',
-			datetime: 'July 01, 2021 11:17:36 AM',
+			datetime: 'November 17, 2023 11:17:36 AM',
 			body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
 		},
 		{
 			id: 4,
 			title: 'My Fourth Post',
-			datetime: 'July 01, 2021 11:17:36 AM',
+			datetime: 'November 17, 2023 11:17:36 AM',
 			body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
 		},
 	]);
+
+	useEffect(() => {
+		const filteredResults = posts.filter(
+			(post) =>
+				post.body.toLowerCase().includes(search.toLowerCase()) ||
+				post.title.toLowerCase().includes(search.toLowerCase())
+		);
+
+		setSearchResults(filteredResults.reverse());
+	}, [posts, search]);
+
+	const history = useHistory();
+
+	const handleDelete = (id) => {
+		const postsList = posts.filter((post) => post.id !== id);
+		setPosts(postsList);
+		history.push('/');
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+		const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+		const newPost = { id, title: postTitle, datetime, body: postBody };
+		const allPosts = [...posts, newPost];
+		setPosts(allPosts);
+		setPostTitle('');
+		setPostBody('');
+		history.push('/');
+	};
 
 	return (
 		<div className="App">
@@ -45,13 +78,19 @@ function App() {
 			<Nav search={search} setSearch={setSearch} />
 			<Switch>
 				<Route exact path="/">
-					<Home posts={posts} />
+					<Home posts={searchResults} />
 				</Route>
 				<Route exact path="/post">
-					<NewPost />
+					<NewPost
+						handleSubmit={handleSubmit}
+						postTitle={postTitle}
+						postBody={postBody}
+						setPostTitle={setPostTitle}
+						setPostBody={setPostBody}
+					/>
 				</Route>
 				<Route path="/post/:id">
-					<PostPage />
+					<PostPage posts={posts} handleDelete={handleDelete} />
 				</Route>
 				<Route path="/about" component={About} />
 				<Route path="*" component={Missing} />
--- src/Missing.js
+++ src/Missing.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 const Missing = () => {
 	return (
-		<main>
-			<h1>Missing</h1>
+		<main className="Missing">
+			<h2>Page Not Found</h2>
+			<p>Well that's dissapointing</p>
+			<p>
+				<Link to="/">Visit our homepage</Link>
+			</p>
 		</main>
 	);
 };
