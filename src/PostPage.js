7bebba6846947aaaaa62a2dd71bad94cf1fffdeb
Austin Amani <45035115+austinamani123@users.noreply.github.com>
2023-11-18 11:43:54 +0300

    Programmed all components and edited the CSS (#5)

--- src/PostPage.js
+++ src/PostPage.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
-const PostPage = () => {
+import { useParams, Link } from 'react-router-dom/cjs/react-router-dom.min';
+const PostPage = ({ handleDelete, posts }) => {
+	const { id } = useParams();
+	const post = posts.find((post) => post.id.toString() === id);
 	return (
-		<main>
-			<h1>PostPage</h1>
+		<main className="PostPage">
+			<article className="post">
+				{post && (
+					<>
+						<h2>{post.title}</h2>
+						<p className="postDate">{post.datetime}</p>
+						<p className="postBody">{post.body}</p>
+						<button onClick={() => handleDelete(post.id)}>Delete Post</button>
+					</>
+				)}
+				{!post && (
+					<>
+						<h2>Post Not Found</h2>
+						<p>Looks like you encountered a problem</p>
+						<p>
+							<Link to="/">Visit our homepage</Link>
+						</p>
+					</>
+				)}
+			</article>
 		</main>
 	);
 };
