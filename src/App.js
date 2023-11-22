cb4114496efc0fa5245533d76a9a82985c79a873
Austin Amani <amaniaustin123@gmail.com>
2023-11-22 22:00:57 +0300

    Created the Update part and finished CRUD Ops Axios

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import api from './api/posts';
+import EditPost from './EditPost';
 
 function App() {
 	const [search, setSearch] = useState('');
 	const [searchResults, setSearchResults] = useState([]);
 	const [postTitle, setPostTitle] = useState('');
 	const [postBody, setPostBody] = useState('');
+	const [editTitle, setEditTitle] = useState('');
+	const [editBody, setEditBody] = useState('');
 	const [posts, setPosts] = useState([]);
 
 	useEffect(() => {
@@ -78,6 +81,20 @@ function App() {
 		}
 	};
 
+	const handleEdit = async (id) => {
+		const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+		const updatedPost = { id, title: editTitle, datetime, body: editBody };
+		try {
+			const response = await api.put(`/posts/${id}`, updatedPost);
+			setPosts(posts.map((post) => (post.id === id ? { ...response.data } : post)));
+			setEditTitle('');
+			setEditBody('');
+			history.push('/');
+		} catch (err) {
+			console.log(`Error: ${err.message}`);
+		}
+	};
+
 	return (
 		<div className="App">
 			<Header title="React JS Blog" />
@@ -93,6 +110,16 @@ function App() {
 						postBody={postBody}
 						setPostTitle={setPostTitle}
 						setPostBody={setPostBody}
+					/>
+				</Route>
+				<Route path="/edit/:id">
+					<EditPost
+						posts={posts}
+						handleEdit={handleEdit}
+						editTitle={editTitle}
+						setEditTitle={setEditTitle}
+						editBody={editBody}
+						setEditBody={setEditBody}
 					/>
 				</Route>
 				<Route path="/post/:id">
