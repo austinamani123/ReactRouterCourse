8d67d505a9c23d6182fd9897bdf768f41ddec5b5
Austin Amani <amaniaustin123@gmail.com>
2023-11-26 10:12:16 +0300

    Created the use AxiosFetch custom hook

--- src/Hooks/useWindowSize.js
+++ src/Hooks/useWindowSize.js
@@ -17,13 +17,7 @@ const useWindowSize = () => {
 		handleResize();
 
 		window.addEventListener('resize', handleResize);
-
-		const cleanUp = () => {
-			console.log('runs if a useEffect dependency changes');
-			window.removeEventListener('resize', handleResize);
-		};
-
-		return cleanUp;
+		return () => window.removeEventListener('resize', handleResize);
 	}, []);
 
 	return windowSize;
