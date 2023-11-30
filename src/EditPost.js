1c68383e7174bccb0d3f6942160e03abc9e0802d
Austin Amani <amaniaustin123@gmail.com>
2023-11-30 16:04:10 +0300

    Cleaned up DataContext(put most logic in comps)

--- src/EditPost.js
+++ src/EditPost.js
@@ -1,13 +1,20 @@
-import { useEffect, useContext } from 'react';
-import { useParams, Link } from 'react-router-dom';
 import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { useParams, useHistory, Link } from 'react-router-dom';
+import format from 'date-fns/format';
+import api from './api/posts';
 import DataContext from './context/DataContext';
 
 const EditPost = () => {
-	const { posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle } =
-		useContext(DataContext);
+	const { posts, setPosts } = useContext(DataContext);
+
+	const [editTitle, setEditTitle] = useState('');
+	const [editBody, setEditBody] = useState('');
+
 	const { id } = useParams();
 	const post = posts.find((post) => post.id.toString() === id);
+
+	const history = useHistory();
 
 	useEffect(() => {
 		if (post) {
@@ -15,6 +22,20 @@ const EditPost = () => {
 			setEditBody(post.body);
 		}
 	}, [post, setEditTitle, setEditBody]);
+
+	const handleEdit = async (id) => {
+		const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+		const updatedPost = { id, title: editTitle, datetime, body: editBody };
+		try {
+			const response = await api.put(`/posts/${id}`, updatedPost);
+			setPosts(posts.map((post) => (post.id === id ? { ...response.data } : post)));
+			setEditTitle('');
+			setEditBody('');
+			history.push('/');
+		} catch (err) {
+			console.log(`Error: ${err.message}`);
+		}
+	};
 
 	return (
 		<main className="NewPost">
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,20 +1,12 @@
 import { createContext, useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { format } from 'date-fns';
-import api from '../api/posts';
-import useWindowSize from '../Hooks/useWindowSize';
 import useAxiosFetch from '../Hooks/useAxiosFetch';
 const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
 	const [search, setSearch] = useState('');
 	const [searchResults, setSearchResults] = useState([]);
-	const [postTitle, setPostTitle] = useState('');
-	const [postBody, setPostBody] = useState('');
-	const [editTitle, setEditTitle] = useState('');
-	const [editBody, setEditBody] = useState('');
 	const [posts, setPosts] = useState([]);
-	const { width } = useWindowSize();
+
 	const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
 	useEffect(() => {
 		const filteredResults = posts.filter(
@@ -26,75 +18,20 @@ export const DataProvider = ({ children }) => {
 		setSearchResults(filteredResults.reverse());
 	}, [posts, search]);
 
-	const history = useHistory();
-
 	useEffect(() => {
 		setPosts(data);
 	}, [data]);
 
-	const handleDelete = async (id) => {
-		try {
-			await api.delete(`/posts/${id}`);
-			const postsList = posts.filter((post) => post.id !== id);
-			setPosts(postsList);
-			history.push('/');
-		} catch (err) {
-			console.log(`Error: ${err.message}`);
-		}
-	};
-
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-		const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-		const newPost = { id, title: postTitle, datetime, body: postBody };
-		try {
-			const response = await api.post('/posts', newPost);
-			const allPosts = [...posts, response.data];
-			setPosts(allPosts);
-			setPostTitle('');
-			setPostBody('');
-			history.push('/');
-		} catch (err) {
-			console.log(`Error: ${err.message}`);
-		}
-	};
-
-	const handleEdit = async (id) => {
-		const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-		const updatedPost = { id, title: editTitle, datetime, body: editBody };
-		try {
-			const response = await api.put(`/posts/${id}`, updatedPost);
-			setPosts(posts.map((post) => (post.id === id ? { ...response.data } : post)));
-			setEditTitle('');
-			setEditBody('');
-			history.push('/');
-		} catch (err) {
-			console.log(`Error: ${err.message}`);
-		}
-	};
-
 	return (
 		<DataContext.Provider
 			value={{
-				width,
 				search,
 				setSearch,
 				searchResults,
 				fetchError,
 				isLoading,
-				handleSubmit,
-				postTitle,
-				setPostTitle,
-				postBody,
-				setPostBody,
 				posts,
-				handleEdit,
-				editBody,
-				setEditBody,
-				editTitle,
-				setEditTitle,
-				handleDelete,
+				setPosts,
 			}}
 		>
 			{children}
